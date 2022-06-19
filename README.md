892ade17bfa9e7b15f6ab656aad1e05d4bfd3eab
gregoryjohncarter <gregoryjohncarter@gmail.com>
2022-06-19 17:29:01 -0400

    comment + vote models w/ relationships

---
+++ README.md
@@ -0,0 +1,7 @@
+## python-newsfeed / just-tech-news
+Message board style app with upvotes. Sourced from instructional content in a 2U boot camp course. 
+
+### Python back end implementation including:
+Py-MySQL, SQLAlchemy, Flask + Jinja2, & bcrypt
+
+
--- app/models/Post.py
+++ app/models/Post.py
@@ -2,9 +2,11 @@ from datetime import datetime
 
 from app.db import Base
 
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from .Vote import Vote
 
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
+
+from sqlalchemy.orm import relationship, column_property
 
 
 class Post(Base):
@@ -24,3 +26,13 @@ class Post(Base):
   updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
 
   user = relationship('User')
+
+  comments = relationship('Comment', cascade='all,delete')
+
+  votes = relationship('Vote', cascade='all,delete')
+
+  vote_count = column_property(
+
+  select([func.count(Vote.id)]).where(Vote.post_id == id)
+
+  )
