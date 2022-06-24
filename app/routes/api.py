9b1113a1cbbae5dd627c54b2f4364425e91573c6
gregoryjohncarter <gregoryjohncarter@gmail.com>
2022-06-24 17:16:26 -0400

    added api POST, PUT, DELETE routes

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify, session
 
-from app.models import User
+from app.models import User, Post, Comment, Vote
 
 from app.db import get_db
 
@@ -8,6 +8,8 @@ import sys
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
+
+# CREATE USER
 @bp.route('/users', methods=['POST'])
 def signup():
   data = request.get_json()
@@ -39,6 +41,7 @@ def signup():
 
   return jsonify(id = newUser.id)
 
+# LOGOUT USER
 @bp.route('/users/logout', methods=['POST'])
 def logout():
   # remove session variables
@@ -46,10 +49,10 @@ def logout():
 
   return '', 204
 
+# LOGIN USER
 @bp.route('/users/login', methods=['POST'])
 def login():
   data = request.get_json()
-
   db = get_db()
 
   try:
@@ -67,3 +70,110 @@ def login():
   session['loggedIn'] = True
 
   return jsonify(id = user.id)
+
+# POST COMMENT
+@bp.route('/comments', methods=['POST'])
+def comment():
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    # create a new comment
+    newComment = Comment(
+      comment_text = data['comment_text'],
+      post_id = data['post_id'],
+      user_id = session.get('user_id')
+    )
+
+    db.add(newComment)
+    db.commit()
+
+  except:
+    print(sys.exc_info()[0])
+    db.rollback()
+    return jsonify(message = 'Comment failed'), 500
+  
+  return jsonify(id = newComment.id)
+
+# UPVOTE 
+@bp.route('/posts/upvote', methods=['PUT'])
+def upvote():
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    # create a new vote with incoming id and session id
+    newVote = Vote(
+      post_id = data['post_id'],
+      user_id = session.get('user_id')
+    )
+
+    db.add(newVote)
+    db.commit()
+
+  except:
+    print(sys.exc_info()[0])
+    db.rollback()
+    return jsonify(message = 'Upvote failed'), 500
+
+  return '', 204
+
+# NEW POST
+@bp.route('/posts', methods=['POST'])
+def create():
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    # create a new post
+    newPost = Post(
+      title = data['title'],
+      post_url = data['post_url'],
+      user_id = session.get('user_id')
+    )
+
+    db.add(newPost)
+    db.commit()
+
+  except:
+    print(sys.exc_info()[0])
+    db.rollback()
+    return jsonify(message = 'Post failed'), 500
+
+  return jsonify(id = newPost.id)
+
+# UPDATE POST
+@bp.route('/posts/<id>', methods=['PUT'])
+def update(id):
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    # retrieve post and update title property
+    post = db.query(Post).filter(Post.id == id).one()
+    post.title = data['title']
+    db.commit()
+
+  except:
+    print(sys.exc_info()[0])
+    db.rollback()
+    return jsonify(message = 'Post not found'), 404
+
+  return '', 204
+
+# DELETE POST 
+@bp.route('/posts/<id>', methods=['DELETE'])
+def delete(id):
+  db = get_db()
+
+  try:
+    # delete post from db
+    db.delete(db.query(Post).filter(Post.id == id).one())
+    db.commit()
+
+  except:
+    print(sys.exc_info()[0])
+    db.rollback()
+    return jsonify(message = 'Post not found'), 404
+
+  return '', 204
