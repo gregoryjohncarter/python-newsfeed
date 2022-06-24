370f9eb03adfadf1bf0f4c804aa9a94ea63578ac
gregoryjohncarter <gregoryjohncarter@gmail.com>
2022-06-24 17:22:16 -0400

    updated data passed to render on dash

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,13 +1,35 @@
 from flask import Blueprint, render_template, session
 
+from app.models import Post
+
+from app.db import get_db
+
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
 @bp.route('/')
 def dash():
-
-  return render_template('dashboard.html', loggedIn=session.get('loggedIn'))
+  db = get_db()
+  posts = (
+    db.query(Post)
+    .filter(Post.user_id == session.get('user_id'))
+    .order_by(Post.created_at.desc())
+    .all()
+  )
+  return render_template(
+  'dashboard.html',
+  posts=posts,
+  loggedIn=session.get('loggedIn')
+  )
 
 @bp.route('/edit/<id>')
 def edit(id):
+  # get single post by id
+  db = get_db()
+  post = db.query(Post).filter(Post.id == id).one()
 
-  return render_template('edit-post.html')
+  # render edit page
+  return render_template(
+    'edit-post.html',
+    post=post,
+    loggedIn=session.get('loggedIn')
+  )
